5c87613de53415828a2fe9fd3c70abf6039d9b8e
adrian.popescu <adrian.popescu@gmail.com>
2026-02-01 17:16:54 +0200

    added cursor pointer inside app

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -23,7 +23,7 @@ export default function TopBar({ showDiff, onToggleDiff }: TopBarProps) {
     <header className="h-14 px-6 flex items-center justify-between bg-[#F8F5F1] border-b border-indigo-900/10 shadow-md ">
       <button onClick={goHome} className="flex items-center gap-0 group">
         {/* Arrow slides in from right and fades in */}
-        <span className="opacity-0 translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 mt-1">
+        <span className="opacity-0 translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300 mt-1 cursor-pointer">
           <SingleLeftArrow width={30} height={30} />
         </span>
 
@@ -32,7 +32,7 @@ export default function TopBar({ showDiff, onToggleDiff }: TopBarProps) {
 
       <button
         onClick={onToggleDiff}
-        className="text-sm px-2 py-1 bg-indigo-100 rounded hover:bg-indigo-200 text-indigo-900"
+        className="text-sm px-2 py-1 bg-indigo-100 rounded hover:bg-indigo-200 text-indigo-900 "
       >
         {showDiff ? "Diff ON" : "Diff OFF"}
       </button>
