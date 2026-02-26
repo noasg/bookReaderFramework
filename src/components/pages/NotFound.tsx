4b0c20378223bee13e19fbddde3bb62b8f5ed904
adrian.popescu <adrian.popescu@gmail.com>
2026-02-26 17:47:13 +0200

    changed texts from 404

--- src/components/pages/NotFound.tsx
+++ src/components/pages/NotFound.tsx
@@ -4,13 +4,13 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
       <h1 className="text-6xl font-bold mb-4">404</h1>
-      <p className="text-xl mb-6">Page not found</p>
+      <p className="text-xl mb-6">Pagina nu a fost gasita</p>
 
       <Link
         to="/"
         className="px-6 py-2 bg-black text-white rounded hover:bg-gray-800 transition"
       >
-        Go back home
+        Inapoi la pagina principala
       </Link>
     </div>
   );
